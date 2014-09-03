35622171cee48dadcede8bb746ecb989372af044
William Hester <williamehester@gmail.com>
2014-09-03 13:36:35 -0500

    Add solution for ChocolateFeast problem.

---
+++ ChocolateFeast/ChocolateFeast.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ChocolateFeast {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            int n = input.nextInt();
+            int c = input.nextInt();
+            int m = input.nextInt();
+
+            int candy = n / c;
+            int wrappers = candy;
+
+            while (wrappers >= m) {
+                int leftoverWrappers = wrappers % m;
+                int newCandy = wrappers / m;
+                candy += newCandy;
+                wrappers = newCandy + leftoverWrappers;
+            }
+
+            System.out.println(candy);
+        }
+    }
+}
